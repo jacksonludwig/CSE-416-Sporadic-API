a478c444dae5fa767cce6b3cc5138d100f2a73eb
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-10-16 23:26:19 -0400

    add test config

---
+++ tests/middleware/auth.test.js
@@ -0,0 +1,8 @@
+describe(`auth unit tests`, () => {
+  const mockToken = "blahblah";
+
+  test(`This should pass (example test)`, (done) => {
+    expect(mockToken).toBe("blahblah");
+    done();
+  });
+});
