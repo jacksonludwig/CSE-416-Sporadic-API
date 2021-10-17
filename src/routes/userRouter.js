0c9e6e22475aa06156cab46b85af259344045256
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-10-17 11:24:11 -0400

    fix: correct path typo

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { validateToken } from "../middleware/authenticate.js";
+import { validateToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
