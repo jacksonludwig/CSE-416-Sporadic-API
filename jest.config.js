f66129bb1bcabf28959af1d7d890130452458fcd
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-10-20 22:00:00 -0400

    fix: stop testing built js

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   rootDir: ".",
+  roots: ["<rootDir>/tests"],
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
 };
