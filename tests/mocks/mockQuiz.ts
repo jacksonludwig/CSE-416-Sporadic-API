cac0935ab04c7226406f9102f5a6a77e1699aa11
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-05 09:53:52 -0400

    chore: add mock quiz for use in multiple tests
    should refactor old tests to use it at some point

---
+++ tests/mocks/mockQuiz.ts
@@ -0,0 +1,26 @@
+import { Quiz } from "../../src/models/Quiz";
+
+const quiz: Quiz = {
+  title: "moviesandstuff",
+  platform: "movies",
+  timeLimit: 5,
+  upvotes: 2,
+  downvotes: 3,
+  description: "this is a quiz where you get to take movies and stuff",
+  _id: "6175e73f27e5d5655ea5805f",
+  questions: [
+    {
+      body: "this is a question, the answer is the first choice",
+      answers: ["0", "1", "2", "3"],
+    },
+    {
+      body: "this is another question, the answer is the second choice",
+      answers: ["0", "1", "2", "3"],
+    },
+  ],
+  correctAnswers: [0, 1],
+  scores: [],
+  comments: [],
+};
+
+export default quiz;
