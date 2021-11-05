8255df435e182cc1d1626e61366044ba322bc671
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-05 00:31:45 -0400

    add full test coverage

--- tests/controllers/submitQuiz.test.ts
+++ tests/controllers/submitQuiz.test.ts
@@ -60,7 +60,7 @@ describe(`submit quiz route tests`, () => {
     UserModel.prototype.update = jest.fn().mockResolvedValueOnce(null);
   });
 
-  test(`Should send back correct answers on success`, async () => {
+  test(`Should send back correct answers and amount correct on success`, async () => {
     const response = await request(app)
       .post(`/quizzes/${mockPlatform}/${mockQuiz.title}/submit`)
       .send({ answers: mockAnswers.answers });
@@ -68,6 +68,17 @@ describe(`submit quiz route tests`, () => {
     expect(validateToken).toHaveBeenCalled();
     expect(response.statusCode).toBe(200);
     expect(response.body).toStrictEqual({ correctAnswers: [0, 1], totalCorrect: 2 });
+  });
+
+  test(`Should send back correct answers and amount correct on success with incorrect answers`, async () => {
+    mockAnswers.answers = [1, 3];
+    const response = await request(app)
+      .post(`/quizzes/${mockPlatform}/${mockQuiz.title}/submit`)
+      .send({ answers: mockAnswers.answers });
+
+    expect(validateToken).toHaveBeenCalled();
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toStrictEqual({ correctAnswers: [0, 1], totalCorrect: 0 });
   });
 
   test(`Should send back 500 if lookup of quiz fails`, async () => {
