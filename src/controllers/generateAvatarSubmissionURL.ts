e3dd0b78c3688d76cd17cd05bc1ae832d56d40b7
Matt <mattsayshi@hotmail.com>
2021-11-13 11:04:41 -0500

    Altered to reflect comments

--- src/controllers/generateAvatarSubmissionURL.ts
+++ src/controllers/generateAvatarSubmissionURL.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import aws from "aws-sdk";
-const region = "us-east-1";
-const bucketName = "sporadic-development-bucket";
+const region = process.env.COGNITO_REGION;
+const bucketName = process.env.AWS_BUCKET;
 const accessKeyId = process.env.AWS_ACCESS_KEY;
 const secretAccessKey = process.env.AWS_SECRET_KEY;
 const s3 = new aws.S3({
@@ -11,15 +11,20 @@ const s3 = new aws.S3({
   signatureVersion: "v4",
 });
 const generateAvatarSubmissionURL = async (req: Request, res: Response) => {
-  console.log(accessKeyId);
   const filename = "avatar.png";
   const params = {
     Bucket: bucketName,
     Key: filename,
     Expires: 60,
   };
-  const uploadURL = await s3.getSignedUrlPromise("putObject", params);
-  return res.send(uploadURL);
+  s3.getSignedUrlPromise("putObject", params)
+    .then((url) => {
+      return res.send(url);
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.sendStatus(500);
+    });
 };
 
 export default generateAvatarSubmissionURL;
