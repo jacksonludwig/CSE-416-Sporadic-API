e779864bce66b2b9e130a79f49d1e345076fc7db
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-15 15:28:54 -0500

    feat: add aboutSection field to user model

--- tests/mocks/mockUser.ts
+++ tests/mocks/mockUser.ts
@@ -8,6 +8,7 @@ const user: User = {
   _id: new ObjectId("6175e73f27e5d5655ea5805f"),
   isGloballyBanned: false,
   lastLogin: new Date(1231231231233),
+  aboutSection: "This is some data about me, I am really great at taking quizzes.",
   subscriptions: [],
   friends: [],
   notifications: [],
