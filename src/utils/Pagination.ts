9e32da95abb6dd614094b24d40f6d7fb2ce72d38
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-23 10:28:52 -0500

    add pagination to retrieve quizzes and quiz feed

--- src/utils/Pagination.ts
+++ src/utils/Pagination.ts
@@ -1,6 +1,7 @@
 /**
  * Convert pageNumber and amountPerPage pagination style to mongodb skip and limit sytle.
- * Returns defaults of page 1 and 100 per page if not page/amount per page is given.
+ *
+ * Returns defaults of page 1 and 100 per page if page/amount per page is not given.
  */
 const pagesToSkipAndLimit = (page?: number, amountPerPage?: number) => {
   if (!page) page = 1;
