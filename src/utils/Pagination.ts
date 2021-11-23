29b90c425b64b826d986eddada67a8d12e8525ea
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-23 10:23:58 -0500

    create pagination util and add to search

---
+++ src/utils/Pagination.ts
@@ -0,0 +1,12 @@
+/**
+ * Convert pageNumber and amountPerPage pagination style to mongodb skip and limit sytle.
+ * Returns defaults of page 1 and 100 per page if not page/amount per page is given.
+ */
+const pagesToSkipAndLimit = (page?: number, amountPerPage?: number) => {
+  if (!page) page = 1;
+  if (!amountPerPage) amountPerPage = 100;
+
+  return { skip: amountPerPage * (page - 1), limit: amountPerPage };
+};
+
+export default pagesToSkipAndLimit;
