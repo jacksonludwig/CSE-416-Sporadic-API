f43e2acf055deffde79e4b188fec0ee9b7f1413d
Matt <mattsayshi@hotmail.com>
2021-12-12 18:57:27 -0500

    added s3 deletion

--- src/controllers/deleteQuizByTitle.ts
+++ src/controllers/deleteQuizByTitle.ts
@@ -2,6 +2,11 @@ import { Request, Response } from "express";
 import PlatformModel from "../models/Platform";
 import QuizModel from "../models/Quiz";
 import UserModel from "../models/User";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+const bucketName = process.env.AWS_BUCKET;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 const deleteQuizByTitle = async (req: Request, res: Response) => {
   const { platform, quizTitle } = req.params;
@@ -37,6 +42,33 @@ const deleteQuizByTitle = async (req: Request, res: Response) => {
     platformObj.pinnedQuizzes = (platformObj.pinnedQuizzes as string[]).filter(
       (q) => q !== quizTitle,
     );
+    const key = `platforms/${req.params.platform}/${req.params.quiz}`;
+
+    const s3Client = new S3Client({
+      region: process.env.COGNITO_REGION,
+      credentials: {
+        accessKeyId: accessKeyId || "",
+        secretAccessKey: secretAccessKey || "",
+      },
+    });
+
+    const deleteCommand = new DeleteObjectCommand({
+      Bucket: bucketName,
+      Key: key,
+    });
+
+    let url: string;
+
+    try {
+      url = await getSignedUrl(s3Client, deleteCommand, { expiresIn: 3600 });
+    } catch (err) {
+      if (err.$metadata && err.$metadata.httpStatusCode < 500)
+        return res.status(err.$metadata.httpStatusCode).send({
+          name: err.name,
+          message: err.message,
+        });
+      throw err;
+    }
 
     await platformObj.update();
 
